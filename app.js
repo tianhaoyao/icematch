8644d9483a8caf500887499d28139c35725ca916
Raya Farhadi <raya.farhadi@gmail.com>
2018-09-15 10:40:49 -0400

    use local ip to run the server so that the server can be accessed through any machine on LAN

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const app = express()
+
+app.use(cors())
+app.use('/static', express.static(path.resolve(__dirname, './static')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './static/index.html'))
+})
+
+module.exports = app
