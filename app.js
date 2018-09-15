f0b9233584e57889b98576484b6b3d652aee02ca
Leo <32031491+tianhaoyao@users.noreply.github.com>
2018-09-15 01:31:21 -0400

    feat(1): Add express in app.js (#16)

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use('/api', myApiRouter)
+
+app.use('/static', express.static(path.join(__dirname, 'static')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/index.html'))
+})
+
+module.exports = app
