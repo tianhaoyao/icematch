d8fbbfa26ed57d918c7e30edf25418c650a12208
Philip Scott <pscott@zeptohost.com>
2018-09-15 01:33:13 -0400

    feat(12): Add register function (#15)

---
+++ game/register.js
@@ -0,0 +1,15 @@
+'use strict'
+
+function register (gameServer) {
+  const roomMap = {
+    'icematch': './room/icematch'
+  }
+
+  for (const roomName in roomMap) {
+    gameServer.register(roomName, require(roomMap[roomName]))
+  }
+
+  return gameServer
+}
+
+module.exports = register
