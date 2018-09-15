91be2dbab502d76989024fae41b70242fc911664
Sokojoe <SokoJoey@Gmail.com>
2018-09-15 16:01:17 -0400

    lol

--- client/view/colyseus.js
+++ client/view/colyseus.js
@@ -3,10 +3,20 @@ import View from './view'
 
 const view = new View()
 const client = new Colyseus.Client('ws://localhost:8080')
-const room = client.join('icematch', { player: true })
+const room = client.join('icematch', { player: false })
 
 room.listen('players/:id', (change) => { updatePlayer(change) })
+room.listen('players/:id/:attribute', (change)=>{updateAttribute()})
 
 function updatePlayer (change) {
+  if (change.operation === "add"){
+    view.createPlayer(change.path.id, change.value.x, change.value.y)
+  }
+  if (change.operation === "remove"){
+    view.removePlayer(change.path.id)
+  }
+}
+
+function updateAttribute(change){
   console.log(change);
 }
