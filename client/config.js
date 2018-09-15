de82e8af342a8ab3d2bf2d05de7c2f2aed3a7a86
Sokojoe <SokoJoey@Gmail.com>
2018-09-15 12:55:26 -0400

    Added pixi view rendering

--- client/config.js
+++ client/config.js
@@ -3,6 +3,6 @@ const SCREEN_WIDTH = 800
 const SCREEN_HEIGHT = 600
 
 module.exports = {
-	SCREEN_WIDTH, 
-	SCREEN_HEIGHT
+  SCREEN_WIDTH,
+  SCREEN_HEIGHT
 }
---
+++ client/view/view.js
@@ -0,0 +1,47 @@
+import * as PIXI from 'pixi.js'
+import * as Resources from '../resources'
+import Player from '../../game/state/player'
+
+class View {
+  constructor () {
+    this.app = new PIXI.Application({backgroundColor: 0xfff8d1})
+    this.players = {}
+    this.sprites = {}
+
+    document.body.appendChild(this.app.view)
+    this.renderLoop()
+  }
+
+  renderLoop () {
+    this.app.ticker.add(() => {
+      for (const id in this.players) {
+        const sprite = this.sprites[id]
+        const player = this.players[id]
+
+        sprite.x = player.x
+        sprite.y = player.y
+      }
+    })
+  }
+
+  createPlayer (id, x, y) {
+    const sprite = PIXI.Sprite.fromImage(Resources.playerSprite)
+
+    sprite.x = x
+    sprite.y = y
+
+    this.players[id] = new Player(0, 0, 'file.png')
+    this.sprites[id] = sprite
+
+    this.app.stage.addChild(sprite)
+  }
+
+  removePlayer (id) {
+    delete this.sprites[id]
+    delete this.players[id]
+  }
+
+  updatePosition (id, dimension, value) {
+    this.players[id][dimension] = value
+  }
+}
