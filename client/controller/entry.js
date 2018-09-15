8eb0f7a16269ad4ca9963c6c1ba4ac1615ac58fa
Philip Scott <pscott@zeptohost.com>
2018-09-15 17:46:00 -0400

    feat(39): Add submit button for image (#57)
    Push to master

--- client/controller/entry.js
+++ client/controller/entry.js
@@ -1,11 +1,15 @@
+/* global FormData */
+
 'use strict'
 
+import * as axios from 'axios'
 import * as Colyseus from 'colyseus.js'
 import TouchController from './controller'
 import virtuals from './virtuals'
 
 const client = new Colyseus.Client(`ws://${window.location.host}`)
 const room = client.join('icematch', { player: true })
+const $ = slt => document.querySelector(slt)
 
 const controller = new TouchController(room, virtuals)
 const touchMap = {
@@ -16,3 +20,15 @@ const touchMap = {
 }
 
 controller.register(touchMap)
+
+$('#upload-btn').addEventListener('click', (e) => {
+  e.preventDefault()
+
+  const $input = $('#upload-input')
+  const data = new FormData()
+
+  data.append('picture', $input.files[0])
+
+  axios.post('/api/upload', data)
+    .then(res => console.log(res.data))
+})
