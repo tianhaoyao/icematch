3fabeab25a9d6eb592da7d7412ded560891f1035
Joey Sokolon <SokoJoey@Gmail.com>
2018-09-15 23:47:20 -0400

    Update redirect.js

--- client/redirect.js
+++ client/redirect.js
@@ -5,12 +5,6 @@ Redirect()
 
 const md = new MobileDetect(window.navigator.userAgent)
 
-window.onload=function(){
-     $(function(){
-         if(window.location.protocol==="https:")
-             window.location.protocol="http";
-     });
- }
 
 if (md.mobile() !== null || md.phone() !== null) {
   window.location.pathname = '/controller'
