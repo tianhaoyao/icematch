9a10af358e42f777429dd056a6fd020a68032bf2
Joey Sokolon <SokoJoey@Gmail.com>
2018-09-16 00:17:00 -0400

    Heroku (#78)
    * Add Procfile
    
    * Add Postinstall script
    
    * Fixed colyseus url
    
    * small fixes
    
    * added logging
    
    * logging
    
    * Removed logging
    
    * Forced redirect to http
    
    * Delete Procfile
    
    * Update redirect.js

---
+++ client/force-http.js
@@ -0,0 +1,9 @@
+function redirectHttp(){
+  window.addEventListener('load', () => {
+    if(window.location.protocol==="https:")
+        window.location.protocol="http"
+  })
+}
+
+
+module.exports = redirectHttp
--- client/redirect.js
+++ client/redirect.js
@@ -1,6 +1,10 @@
 import MobileDetect from 'mobile-detect'
+import Redirect from './force-http'
+
+Redirect()
 
 const md = new MobileDetect(window.navigator.userAgent)
+
 
 if (md.mobile() !== null || md.phone() !== null) {
   window.location.pathname = '/controller'
